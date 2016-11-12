994017dda26a658a7136ec07da3a31763629345b
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-11-13 03:54:51 +0900

    Initial Commit

---
+++ xcode-project-test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xcode-project-test
+//
+//  Created by 西脇友一 on 2016/11/13.
+//  Copyright © 2016年 西脇友一. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
